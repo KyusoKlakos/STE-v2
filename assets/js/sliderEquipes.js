f0bc464b9ad92154f439149dc86a45fd8ebbe543
Kevin <thewizardytp@gmail.com>
2019-12-05 17:35:30 +0100

    Intégration pour les equipes

--- assets/js/sliderEquipes.js
+++ assets/js/sliderEquipes.js
@@ -1,17 +1,23 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+let slideIndex = 1;
+let slides = document.getElementsByClassName("mySlides");
+slides[0].style.display = "block";
+
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  let i;
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-  x[slideIndex-1].style.display = "block";  
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].getElementsByClassName("content")[0].classList.add("activeSlide");
 }   
